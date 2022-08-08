649f07982484587585dd0724d8930e532e18ed88
RichardLeonardoSavage <savagerichard100@gmail.com>
2022-08-08 14:13:29 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace BasicForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is an array, luckyNumbers is the collection of variables
+            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
+            
+            for (int i = 0; i < luckyNumbers.Length; i++)
+            {
+                Console.WriteLine(luckyNumbers[i]);
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
